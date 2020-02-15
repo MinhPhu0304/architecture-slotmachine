62192888daf876a4d25142a503f5c432a23d1f04
Minh-Phu <Lminhphu0304@gmail.com>
2020-02-16 12:42:57 +1300

    Remove console log

--- js/index.js
+++ js/index.js
@@ -6,11 +6,8 @@ startBtn.addEventListener('click', showArchitecture)
 
 function showArchitecture () {
     const backEndIndex = Math.floor(Math.random() * Math.floor(backendTechs.length))
-    console.log(backendTechs[backEndIndex])
     const frontEnd = Math.floor(Math.random() * Math.floor(frontEndTechs.length))
-    console.log(frontEndTechs[frontEnd])
     const database = Math.floor(Math.random() * Math.floor(databases.length))
-    console.log(databases[database])
     renderResult(backendTechs[backEndIndex], frontEndTechs[frontEnd], databases[database])
 }
 
