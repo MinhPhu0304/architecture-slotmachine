048b20c3edf1aa1b1f7a7c9cbf917920461b2595
Minh-Phu <Lminhphu0304@gmail.com>
2020-02-16 12:34:14 +1300

    Implement render results function

--- js/index.js
+++ js/index.js
@@ -11,4 +11,22 @@ function showArchitecture () {
     console.log(frontEndTechs[frontEnd])
     const database = Math.floor(Math.random() * Math.floor(databases.length))
     console.log(databases[database])
-}   
+    await renderResult(backendTechs[backEndIndex], frontEndTechs[frontEnd], databases[database])
+}
+
+function renderResult (backend, frontend, database) {
+    const [resultContainer] = document.getElementsByClassName('result-container')
+    const frontEndTxtNode = document.createElement('p')
+    frontEndTxtNode.textContent = frontend
+
+    const backEndTxtNode = document.createElement('p')
+    backEndTxtNode.textContent = backend
+    const databaseTxtNode = document.createElement('p')
+    databaseTxtNode.textContent = database
+    const docFragment = document.createDocumentFragment()
+    docFragment.appendChild(frontEndTxtNode)
+    docFragment.appendChild(backEndTxtNode)
+    docFragment.appendChild(databaseTxtNode)
+
+    resultContainer.appendChild(docFragment)
+}
