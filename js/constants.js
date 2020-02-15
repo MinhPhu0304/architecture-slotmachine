3ae37a6a71f02c4c1610b24d2a7ebb39ce2e98cb
Minh-Phu <Lminhphu0304@gmail.com>
2020-02-16 11:19:26 +1300

    Add list of techs

---
+++ js/constants.js
@@ -0,0 +1,39 @@
+const backendTechs = [
+ 'Ruby on Rails',
+ '.NET Core',
+ 'ExpressJS',
+ 'Django',
+ 'Flask',
+ 'PHP',
+ 'Laravel',
+ 'Spring Boot'
+]
+
+const frontEndTechs = [
+    'React',
+    'AngularJS',
+    'Angular',
+    'VueJS',
+    'Vanila JS',
+    'Ember',
+    'jQuery',
+    'Backbone JS',
+    'Svelte'
+]
+
+const databases = [
+    'PostgreSQL',
+    'MySQL',
+    'MongoDB',
+    'DynamoDB',
+    'SQLite',
+    'Maria DB',
+    'CouchDB',
+    'ArangoDB'
+]
+
+export {
+    backendTechs,
+    databases,
+    frontEndTechs,
+}
