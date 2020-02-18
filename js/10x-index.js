d65f8e79d29239e93372bb30af5d71bb7849edcf
Walter Lim <waltissomewhere@gmail.com>
2020-02-18 21:10:53 +1300

    added extra challenges

---
+++ js/10x-index.js
@@ -0,0 +1,109 @@
+import {
+  backendTechs,
+  databases,
+  frontEndTechs,
+  projectManagement,
+  editorTech
+} from './10x-constants'
+
+const startBtn = document.getElementById('start')
+let timmerId
+
+startBtn.addEventListener('click', showArchitecture)
+
+function showArchitecture () {
+  const backEndIndex = Math.floor(
+    Math.random() * Math.floor(backendTechs.length)
+  )
+  const frontEnd = Math.floor(Math.random() * Math.floor(frontEndTechs.length))
+  const database = Math.floor(Math.random() * Math.floor(databases.length))
+  const editor = Math.floor(Math.random() * Math.floor(editorTech.length))
+  const manager = Math.floor(Math.random() * Math.floor(projectManagement.length))
+  renderResult(
+    backendTechs[backEndIndex],
+    frontEndTechs[frontEnd],
+    databases[database],
+    editorTech[editor],
+    projectManagement[manager]
+  )
+
+  if (!timmerId) timmerId = setInterval(showArchitecture, 100)
+  setTimeout(() => {
+    clearInterval(timmerId)
+    timmerId = null
+  }, 2000)
+}
+
+function renderResult (backend, frontend, database, editor, manager) {
+  const [resultContainer] = document.getElementsByClassName('result-container')
+  clearChildNode(resultContainer)
+
+  //Define slot machine Titles
+  const frontEndTitleTxtNode = createTextElement('h4', 'Frontend')
+  const backEndTitleTxtNode = createTextElement('h4', 'Backend')
+  const DatabaseTitleTxtNode = createTextElement('h4', 'Database')
+  const editorTitleTxtNode = createTextElement('h4', 'IDE/Editor')
+  const managerTitleTxtNode = createTextElement('h4', 'Project Management')
+
+  //Slot Machine Content
+
+  const frontEndTxtNode = createTextElement('h3', frontend)
+  frontEndTxtNode.classList.add('frontend')
+
+  const backEndTxtNode = createTextElement('h3', backend)
+  backEndTxtNode.classList.add('backend')
+
+  const databaseTxtNode = createTextElement('h3', database)
+  databaseTxtNode.classList.add('db')
+
+  const editorTxtNode = createTextElement('h3', editor)
+  databaseTxtNode.classList.add('edit')
+
+  const managementTxtNode = createTextElement('h3', manager)
+  databaseTxtNode.classList.add('mgmt')
+
+  // Combine title and output
+
+  const frontEndDiv = combineChildIntoDiv(frontEndTitleTxtNode, frontEndTxtNode)
+  frontEndDiv.classList.add('frontend')
+
+  const backendDiv = combineChildIntoDiv(backEndTitleTxtNode, backEndTxtNode)
+  backendDiv.classList.add('backend')
+
+  const databaseDiv = combineChildIntoDiv(DatabaseTitleTxtNode, databaseTxtNode)
+  databaseDiv.classList.add('db')
+
+  const editorDiv = combineChildIntoDiv(editorTitleTxtNode, editorTxtNode)
+  editorDiv.classList.add('edit')
+
+  const managerDiv = combineChildIntoDiv(managerTitleTxtNode, managementTxtNode)
+  managerDiv.classList.add('mgmt')
+
+  const docFragment = document.createDocumentFragment()
+  docFragment.appendChild(frontEndDiv)
+  docFragment.appendChild(backendDiv)
+  docFragment.appendChild(databaseDiv)
+  docFragment.appendChild(editorDiv)
+  docFragment.appendChild(managerDiv)
+  resultContainer.appendChild(docFragment)
+}
+
+function clearChildNode (node) {
+  while (node.firstChild) {
+    node.firstChild.remove()
+  }
+}
+
+function createTextElement (tagName = 'p', content) {
+  const element = document.createElement(tagName)
+  element.textContent = content
+  return element
+}
+
+function combineChildIntoDiv (...childs) {
+  const container = document.createElement('div')
+
+  childs.forEach(childNode => container.appendChild(childNode))
+
+  return container
+}
